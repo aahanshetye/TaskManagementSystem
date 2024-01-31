24fa3ea619f18a652f9c1caf5dbbcc0819147428
vihankumbhare1234 <vihanak05@gmail.com>
2024-01-31 11:33:53 +0530

    Login Changed

---
+++ login.js
@@ -0,0 +1,7 @@
+let loginButton = document.querySelector("#loginbtn");
+
+function myFunction() {
+    alert("Please fill out the details.")
+}
+
+loginButton.addEventListener("onclick", myFunction);
